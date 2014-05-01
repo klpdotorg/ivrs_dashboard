630b07c20200e999d4359bd2d630812cbb2945d0
afzal-lakdawala <al@pykih.com>
2014-05-01 16:46:29 +0530

    added all data

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,12 +3,12 @@ class Response < ActiveRecord::Base
 
   before_save :trim_strings
 
+
   def self.seed  
     Response.delete_all  
     csv_text = File.read('public/response.csv')
     csv = CSV.parse(csv_text, :headers => true)    
     csv.each do |row|
-      puts row.to_hash
       Response.create!(row.to_hash)
     end    
   end
